fd112403add4a410a8d3a0182db2a6ad25556c54
pmkwembe023 <pmkwembe023@student.wethinkcode.co.za>
2024-07-31 18:30:24 +0200

    push

--- backend/models/databaseConnect.js
+++ backend/models/databaseConnect.js
@@ -6,7 +6,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
     connectionLimit: process.env.DB_LIMIT,
     queueLimit: 0
   });
